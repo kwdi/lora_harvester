c2d22592d91fc061d5ffadfbe8c0484c8b5719a8
kwdi <kostas.droid@gmail.com>
2019-04-25 16:51:16 +0200

    sleepmode and voltage

--- hello_LoRa.ino
+++ hello_LoRa.ino
@@ -1,19 +1,33 @@
-// Hello LoRa - ABP TTN Packet Sender (Multi-Channel)
-// Tutorial Link: https://learn.adafruit.com/the-things-network-for-feather/using-a-feather-32u4
-//
-// Adafruit invests time and resources providing this open source code.
-// Please support Adafruit and open source hardware by purchasing
-// products from Adafruit!
-//
-// Copyright 2015, 2016 Ideetron B.V.
-//
-// Modified by Brent Rubell for Adafruit Industries, 2018
+// Kdi-ttn
 /************************** Configuration ***********************************/
 #include <TinyLoRa.h>
 #include <SPI.h>
+#include <Adafruit_SleepyDog.h>
 
-// Visit your thethingsnetwork.org device console
-// to create an account, or if you need your session keys.
+#define VBATPIN A7
+
+// Radio Settings
+#define Datarate SF7BW125   /*  SF7BW125
+                                SF7BW250 
+                                SF8BW125
+                                SF9BW125
+                                SF10BW125
+                                SF11BW125
+                                SF12BW125 */
+
+#define Channel MULTI       /*  CH0
+                                CH1
+                                CH2
+                                CH3
+                                CH4
+                                CH5
+                                CH6
+                                CH7
+                                MULTI */
+
+#define TxPower 14          // 5..20
+
+
 
 // Network Session Key (MSB)
 uint8_t NwkSkey[16] = { 0x03, 0xFE, 0xEA, 0xA4, 0x21, 0x7B, 0x0D, 0xEB, 0x6E, 0x7C, 0x16, 0xDE, 0x56, 0xB5, 0x8E, 0x50 };
@@ -24,7 +38,6 @@ uint8_t AppSkey[16] = { 0x28, 0x60, 0x81, 0x13, 0xE1, 0x0C, 0xAB, 0xB0, 0x90, 0x
 // Device Address (MSB)
 uint8_t DevAddr[4] = { 0x26, 0x01, 0x1B, 0xCE };
 
-/************************** Example Begins Here ***********************************/
 // Data Packet to Send to TTN
 unsigned char loraData[11] = {"hello LoRa"};
 
@@ -34,47 +47,51 @@ const unsigned int sendInterval = 30;
 // Pinout for Adafruit Feather 32u4 LoRa
 TinyLoRa lora = TinyLoRa(7, 8);
 
-// Pinout for Adafruit Feather M0 LoRa
-//TinyLoRa lora = TinyLoRa(3, 8);
 
 void setup()
 {
-  delay(2000);
+  //delay(2000);
   //Serial.begin(9600);
-  //while (! Serial);
-  
+   
   // Initialize pin LED_BUILTIN as an output
   pinMode(LED_BUILTIN, OUTPUT);
   
   // Initialize LoRa
-  Serial.print("Starting LoRa...");
   // define multi-channel sending
-  lora.setChannel(MULTI);
+  lora.setChannel(Channel);
   // set datarate
-  lora.setDatarate(SF7BW125);
+  lora.setDatarate(Datarate);
   // set TxPower
-  lora.setTxPower(23);
+  lora.setTxPower(TxPower);
+
   if(!lora.begin())
   {
-    //Serial.println("Failed");
-    //Serial.println("Check your radio");
+    digitalWrite(LED_BUILTIN, HIGH);
     while(true);
   }
-  //Serial.println("OK");
+
 }
 
 void loop()
 {
-  //Serial.println("Sending LoRa Data...");
-  lora.sendData(loraData, sizeof(loraData), lora.frameCounter);
-  //Serial.print("Frame Counter: ");Serial.println(lora.frameCounter);
-  lora.frameCounter++;
 
-  // blink LED to indicate packet sent
-  //digitalWrite(LED_BUILTIN, HIGH);
-  //delay(100);
-  //digitalWrite(LED_BUILTIN, LOW);
-  
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024; // convert to voltage
+
+  // If battery is below 3.5 go to sleep and check again in 8 seconds
+//  while(measuredvbat < 3.5){
+//   Watchdog.sleep(8000);
+// }
+
+  lora.sendData(loraData, sizeof(loraData), lora.frameCounter);
+  lora.frameCounter++;
+  Watchdog.sleep(4000);
+
   //Serial.println("delaying...");
-  delay(sendInterval * 100);
+  //delay(sendInterval * 100);
+
+  //Serial.print("VBat: " ); Serial.println(measuredvbat);
+
 }
